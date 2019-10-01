532310bff9543e1674e2907fa9f7dd2d5f72a7ec
Laith Youtuber <49840784+laith98@users.noreply.github.com>
2019-10-01 09:09:01 -0700

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,35 @@
+/**
+	* @module config/bot
+	* @desc Before editing, this file should be cloned from bot.js.template to bot.js
+	* @author WizardCM <bots@wizardcm.com>
+*/
+
+/**
+	@type {Object.<string, string>}
+*/
+module.exports = {
+	/**
+	 * @desc Bot username without discriminator
+	 * @default "MyTimeBot"
+	 * @type string
+	 */
+	name: "MyTimeBot",
+	/**
+	 * @desc Global bot command prefix
+	 * @default "?"
+	 * @type string
+	*/
+	prefix: "-+",
+	/** 
+	 * @desc Discord API bot token (keep it secret)
+	 * @default "InsertYourTokenHere"
+	 * @type string
+	 */
+	token: "NjI4NjI0MDk5ODkwOTU0MjU5.XZN55w.9EbbZjWm6cnmRsaMk0rgovlSbmo",
+	/** 
+	 * @desc Global bot response heading
+	 * @default "\uD83D\uDD52 Discord Time Bot"
+	 * @type string
+	 */
+	title: "\uD83D\uDD52 Discord Time Bot"
+};
